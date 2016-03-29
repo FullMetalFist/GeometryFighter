2177ba7cf7b1470c103f413ee2fa52930f7b91a1
Michael Vilabrera <m.vilabrera@gmail.com>
2016-03-28 21:23:22 -0400

    initial commit, chapter 1

---
+++ GeometryFighter/GameViewController.swift
@@ -0,0 +1,39 @@
+//
+//  GameViewController.swift
+//  GeometryFighter
+//
+//  Created by Michael Vilabrera on 3/28/16.
+//  Copyright (c) 2016 Giving Tree. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class GameViewController: UIViewController {
+    
+    var scnView: SCNView!
+    var scnScene: SCNScene!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupView()
+        self.setupScene()
+    }
+    
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
+    func setupView() {
+        scnView = self.view as! SCNView
+    }
+    
+    func setupScene() {
+        scnScene = SCNScene()
+        scnView.scene = scnScene
+    }
+}
